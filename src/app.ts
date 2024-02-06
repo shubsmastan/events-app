8576e5c25c9a6f114112cbba3c5e38fc53e19ca6
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2024-02-06 20:10:13 +0000

    feat: set up graphQL queries and mutations

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from "express";
 // import bodyParser from "body-parser";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+import fs from "fs";
 
 const app = express();
 
@@ -7,6 +10,26 @@ app.get("/", (_, res) => {
   res.send("Hello Faerun!");
 });
 
+app.use(
+  "/api",
+  graphqlHTTP({
+    schema: buildSchema(
+      `${fs.readFileSync(require.resolve("./schema.graphql"), "utf-8")}`
+    ),
+    rootValue: {
+      events: () => {
+        return ["One", "Two", "Three"];
+      },
+      createEvent: ({ name }: { name: string }) => {
+        return name;
+      },
+    },
+    graphiql: true,
+  })
+);
+
 const PORT = process.env.PORT || 3300;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
